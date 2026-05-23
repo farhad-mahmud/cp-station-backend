d8fbec6a14f67cb4042fd43699812c8a42dc64bd
Farhad Mahmud <rixonahmed@gmail.com>
2026-05-24 02:40:30 +0600

    backend

---
+++ src/GetResourcesByTopicsHandler.java
@@ -0,0 +1,90 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+
+// backend api to get resources according to topics from database ..
+public class GetResourcesByTopicsHandler implements HttpHandler {
+
+    public void handle(HttpExchange exchange) {
+
+        try {
+
+            String dburl = "jdbc:postgresql://localhost:5432/postgres";
+            String user = "farhadmahmud";
+            String password = "1234";
+
+            // extract query from uri .. 
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver loaded successfully");
+
+            String query = exchange.getRequestURI().getQuery();
+            
+            String topic = URLDecoder.decode(query.split("=")[1], StandardCharsets.UTF_8);
+
+            Connection conn = DriverManager.getConnection(dburl, user, password);
+
+            String sql =
+                "SELECT r.id, r.title ,r.url , r.type " +
+                "FROM resources r " +
+                "JOIN resource_topics rt ON r.id = rt.resource_id " +
+                "JOIN topics t ON t.id = rt.topic_id " +
+                "WHERE t.name = ?";
+
+            // preparedstatement is sequred this is how java sends sql query ..
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, topic);
+            ResultSet rs = stmt.executeQuery();
+
+            String response = "[";
+
+            boolean first = true;
+
+
+            while (rs.next()) {
+
+                if (!first) response += ",";
+                 first = false;
+
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String url = rs.getString("url");
+                String type = rs.getString("type");
+
+                response += "{";
+                response += "\"id\":" + id + ",";
+                response += "\"title\":\"" + title + "\",";
+                response += "\"url\":\"" + url + "\",";
+                response += "\"type\":\"" + type + "\"";
+                    response += "}";
+                 }
+
+            response += "]";
+
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+              String error = "Server error: " + e.getMessage();
+
+              
+        try {
+        exchange.getResponseHeaders().set("Content-Type", "text/plain");
+        exchange.sendResponseHeaders(500, error.getBytes().length);
+        exchange.getResponseBody().write(error.getBytes());
+        exchange.getResponseBody().close();
+         } catch (Exception ignored) {}
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,65 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+
+        // creating route /endpoint = similar to framework
+        server.createContext("/", new RootHandler());
+
+        // get resource by topics api .. in server . 
+
+        server.createContext("/resources-by-topic", new GetResourcesByTopicsHandler());
+        // get topics.. by api
+
+        server.createContext("/topics" , new GetTopicsHandler());
+
+        //thread executor..
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port 8080");
+    }
+
+    // this class handles requests.. 
+    static class RootHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) {
+            try {
+
+                String response = "CP Backend is running";
+
+                // sending http status..
+                // status code 200 = ok,
+                // 404 = not found..
+                exchange.sendResponseHeaders(200, response.length());
+
+                // gets pipe to browser ..
+                OutputStream os = exchange.getResponseBody();
+                //converts string to bytes..
+                os.write(response.getBytes());
+                // closing stream
+                os.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                  String error = "Server error: " + e.getMessage();
+
+            try {
+                exchange.sendResponseHeaders(500, error.length());
+                exchange.getResponseBody().write(error.getBytes());
+                exchange.getResponseBody().close();
+                } catch (Exception ignored) {}
+            }   
+        }
+    }
+
+    
+
+}
